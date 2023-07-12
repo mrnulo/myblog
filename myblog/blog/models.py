b9433b994c3c5b9bb7ecbddc775965194127b624
Amir <69367070+mrnulo@users.noreply.github.com>
2023-07-12 16:29:43 -0700

    Add files via upload

---
+++ myblog/blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import CharField
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=110, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
